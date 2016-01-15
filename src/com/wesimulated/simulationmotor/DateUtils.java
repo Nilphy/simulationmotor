b34b2ee3aec71aff5947f148ff302e8dcb05de6e
Carolina Soledad Galli <carogalli@gmail.com>
2016-01-15 01:12:44 +0100

    Add DateUtils

---
+++ src/com/wesimulated/simulationmotor/DateUtils.java
@@ -0,0 +1,22 @@
+package com.wesimulated.simulationmotor;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateUtils {
+
+	public static Date convertToStartOfNextLabDay(Date date) {
+		Calendar calOfDate = Calendar.getInstance();
+		calOfDate.setTime(date);
+		Calendar cal = Calendar.getInstance();
+		cal.set(calOfDate.get(Calendar.YEAR), calOfDate.get(Calendar.MONTH), calOfDate.get(Calendar.DATE), 9, 0);
+		if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.FRIDAY) {
+			cal.add(Calendar.DATE, 3);
+		} else if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
+			cal.add(Calendar.DATE, 2);
+		} else {
+			cal.add(Calendar.DATE, 1);			
+		}
+		return cal.getTime();
+	}
+}
