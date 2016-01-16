73f8c63f81d64f5a9037e616d9bd57be2ed299d9
Carolina Soledad Galli <carogalli@gmail.com>
2016-01-16 17:07:18 +0100

    move ejecution of simulator to a new thread to pause it with a semaphore when is waiting for hla grants

--- src/com/wesimulated/simulation/BaseExcecutor.java
+++ src/com/wesimulated/simulation/BaseExcecutor.java
@@ -1,5 +1,6 @@
 package com.wesimulated.simulation;
 
+import java.util.concurrent.Semaphore;
 
 import javafx.application.Platform;
 import hla.rti1516e.exceptions.FederateNotExecutionMember;
@@ -13,31 +14,66 @@ import hla.rti1516e.exceptions.RequestForTimeRegulationPending;
 import hla.rti1516e.exceptions.RestoreInProgress;
 import hla.rti1516e.exceptions.SaveInProgress;
 
+import com.wesimulated.simulation.hla.DateLogicalTime;
 import com.wesimulated.simulation.runparameters.EndCodition;
+import com.wesimulated.simulationmotor.operationbased.TimeControllerEntity;
 
 public abstract class BaseExcecutor {
 	private EndCodition endCondition;
 	private Clock clock;
-	
+	private Semaphore hlaWait;
+
 	public BaseExcecutor(EndCodition endCondition) {
 		this.endCondition = endCondition;
+		this.hlaWait = new Semaphore(0);
 	}
 
 	protected abstract void doCicle() throws LogicalTimeAlreadyPassed, InvalidLogicalTime, InTimeAdvancingState, RequestForTimeRegulationPending, RequestForTimeConstrainedPending, SaveInProgress,
 			RestoreInProgress, FederateNotExecutionMember, NotConnected, RTIinternalError, InterruptedException;
-	
-		while (!this.simulationMustStop()) {
-			this.doCicle();
-		}
+
 	public void run() throws LogicalTimeAlreadyPassed, InvalidLogicalTime, InTimeAdvancingState, RequestForTimeRegulationPending, RequestForTimeConstrainedPending, SaveInProgress, RestoreInProgress,
 			FederateNotExecutionMember, NotConnected, RTIinternalError, InterruptedException {
+		Platform.runLater(() -> {
+			try {
+				if (this.getClock() == null) {
+					this.pauseSimulationMotorExecution();
+				}
+				while (!this.isSimulationEnd()) {
+					this.doCicle();
+				}
+			} catch (LogicalTimeAlreadyPassed | InvalidLogicalTime | InTimeAdvancingState | RequestForTimeRegulationPending | RequestForTimeConstrainedPending | SaveInProgress | RestoreInProgress
+					| FederateNotExecutionMember | NotConnected | RTIinternalError | InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		});
 	}
-	
-	protected boolean simulationMustStop() {
+
+	public boolean isSimulationEnd() {
 		return endCondition.isSatisfied();
 	}
 
+	public void initClock(DateLogicalTime time, TimeControllerEntity timeControllerEntity) {
+		this.setClock(new Clock(time.getValue(), this, timeControllerEntity));
+		this.continueSimulationMotorExecution();
+	}
 
+	public void continueFromDate(DateLogicalTime time) {
+		if (this.getClock() != null && this.getClock().getCurrentDate().compareTo(time.getValue()) != 0) {
+			System.out.println("Seting already setted date to clock with a different value, the difference is of new - old: "
+					+ (time.getValue().getTime() - this.getClock().getCurrentDate().getTime()));
+		}
+		this.getClock().setCurrentDate(time.getValue());
+		this.continueSimulationMotorExecution();
+	}
+
+	// TODO: this has to be called from the clock when it requests time advance
+	public void pauseSimulationMotorExecution() throws InterruptedException {
+		this.hlaWait.acquire();
+	}
+
+	public void continueSimulationMotorExecution() {
+		this.hlaWait.release();
+	}
 
 	public Clock getClock() {
 		return clock;
