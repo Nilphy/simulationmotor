6feb130f9ad871c3e658d565c251120fdfd43ffb
Carolina Soledad Galli <carogalli@gmail.com>
2016-09-10 23:29:22 +0200

    This class is abstract

--- src/com/wesimulated/simulationmotor/des/OperationBasedSimulator.java
+++ src/com/wesimulated/simulationmotor/des/OperationBasedSimulator.java
@@ -2,7 +2,7 @@ package com.wesimulated.simulationmotor.des;
 
 import com.wesimulated.simulation.BaseSimulator;
 
-public class OperationBasedSimulator extends BaseSimulator {
+public abstract class OperationBasedSimulator extends BaseSimulator {
 
 	public OperationBasedExecutor getOperationBasedExecutor() {
 		return (OperationBasedExecutor) this.getExecutor();
