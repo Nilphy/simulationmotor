629ca497cf1aba10a6206a2a21a17178cb04efd8
Carolina Soledad Galli <carogalli@gmail.com>
2016-09-10 23:30:21 +0200

    Add new methods used in simulator construction

--- src/com/wesimulated/simulationmotor/systemdynamics/SystemDynamicsSimulator.java
+++ src/com/wesimulated/simulationmotor/systemdynamics/SystemDynamicsSimulator.java
@@ -7,4 +7,17 @@ public abstract class SystemDynamicsSimulator extends BaseSimulator {
 	public SystemDynamicsExecutor getSystemDynamicsExecutor() {
 		return (SystemDynamicsExecutor) this.getExecutor();
 	}
+
+	public void register(Module module) {
+		this.getSystemDynamicsExecutor().addFlows(module.getFlows());
+		this.getSystemDynamicsExecutor().addStocks(module.getStocks());
+	}
+
+	public void register(Stock stock) {
+		this.getSystemDynamicsExecutor().add(stock);
+	}
+
+	public void register(Flow flow) {
+		this.getSystemDynamicsExecutor().add(flow);
+	}
 }
