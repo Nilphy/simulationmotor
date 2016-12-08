d4b54e113317b77dafa92d6664f94173664cc750
Carolina Soledad Galli <carogalli@gmail.com>
2016-12-08 16:35:28 +0100

    Add state to model simulation as states machines

---
+++ src/com/wesimulated/simulationmotor/des/State.java
@@ -0,0 +1,14 @@
+package com.wesimulated.simulationmotor.des;
+
+/**
+ * La simulación en eventos discretos puede pensarse como un modelado del
+ * sistema en estudio en una máquina de estados.
+ * 
+ * Esta es la interfaz que deben implementar todos los estados.
+ * 
+ * @author Carolina
+ * 
+ */
+public interface State {
+
+}
