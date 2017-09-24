f26d518393a592a64eccbb2825c034dc6355efee
Carolina Soledad Galli <carogalli@gmail.com>
2017-09-24 11:32:29 +0200

    Add more util method to handle dates

--- src/com/wesimulated/simulationmotor/DateUtils.java
+++ src/com/wesimulated/simulationmotor/DateUtils.java
@@ -1,5 +1,9 @@
 package com.wesimulated.simulationmotor;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -81,4 +85,19 @@ public class DateUtils {
 		END_MINUTE = minute;
 	}
 
+	public static Date asDate(LocalDate localDate) {
+		return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+	}
+
+	public static Date asDate(LocalDateTime localDateTime) {
+		return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+	}
+
+	public static LocalDate asLocalDate(Date date) {
+		return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+	}
+
+	public static LocalDateTime asLocalDateTime(Date date) {
+		return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+	}
 }
