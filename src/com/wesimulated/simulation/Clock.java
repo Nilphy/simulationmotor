ecb51ee5070a21bb3fd8de15e75ab3484ebc387e
Carolina Soledad Galli <carogalli@gmail.com>
2017-11-02 23:35:13 +0100

    clone the date when getting the current date as it will be used as a timestamp

--- src/com/wesimulated/simulation/Clock.java
+++ src/com/wesimulated/simulation/Clock.java
@@ -6,28 +6,27 @@ import java.util.Date;
 import com.wesimulated.simulationmotor.des.TimeControllerEntity;
 
 public class Clock {
-	
+
 	private Date date;
 	private Date startDate;
 	private BaseExecutor simulationExecutor;
 	private TimeControllerEntity timeControllerEntity;
-	
-	
+
 	public Clock(Date startDate, BaseExecutor simulationExecutor, TimeControllerEntity timeControllerEntity) {
 		this.startDate = startDate;
 		this.setCurrentDate(startDate);
 		this.simulationExecutor = simulationExecutor;
 		this.timeControllerEntity = timeControllerEntity;
 	}
-	
+
 	public boolean durationHasPassed(Duration duration) {
 		return date.getTime() - startDate.getTime() > duration.toMillis();
 	}
-	
+
 	public boolean hasThisDate(Date thisDate) {
 		return this.date.equals(thisDate);
 	}
-	
+
 	public boolean dateHasPassed(Date date) {
 		return this.date.compareTo(date) > 0;
 	}
@@ -38,7 +37,7 @@ public class Clock {
 	}
 
 	public Date getCurrentDate() {
-		return date;
+		return (Date) date.clone();
 	}
 
 	public void setCurrentDate(Date date) {
